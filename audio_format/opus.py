c21fd91ac8c1db2da6378b788616b387fc19787d
hufanglei <690328661@qq.com>
2025-11-08 10:54:24 +0800

    feat(audio): 实现音频转OPUS功能并完善帧处理逻辑
    - 引入numpy库用于音频数据处理
    - 添加OPUS编码帧时间配置注释
    - 补全audio_to_opus方法的代码注释
    - 实现按帧分割和补零处理PCM数据
    - 使用numpy优化音频帧数据转换- 返回编码后的OPUS数据列表及音频时长
    - 添加主函数测试音频转码功能

--- audio_format/opus.py
+++ audio_format/opus.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import opuslib_next
 from pydub import AudioSegment
 
@@ -12,9 +13,9 @@ class Opus_Encode:
         self.opus_sample_rate = 16000
         self.opus_channel = 1
         self.opus_sample_width = 2
-        self.opus_frame_time = 60
+        self.opus_frame_time = 60 # 60ms
 
-
+    # 将下面的代码补全注释
     def audio_to_opus(self, audio_file_path):
         file_type = os.path.splitext(audio_file_path)[1]
         if file_type:
@@ -36,7 +37,36 @@ class Opus_Encode:
 
         encoder = opuslib_next.Encoder(self.opus_sample_rate, self.opus_channel, opuslib_next.APPLICATION_AUDIO)
 
-        opus_data = []
+        # 获取每一帧的O采样数
+        frame_num = int(self.opus_sample_rate / 1000 * self.opus_frame_time)
 
-        for i in range(0, len(raw_data), 960):
-            opus_data.append(encoder.encode(raw_data[i:i+48000], 48000))
+        # 计算每帧的采样字节数
+        frame_bytes_size = frame_num * self.opus_channel * self.opus_sample_width
+
+        opus_datas = []
+
+        for i in range(0, len(raw_data), frame_bytes_size):
+            # 获取当前帧的PCM数据
+            chunk = raw_data[i:i+frame_bytes_size]
+            # 计算当前帧长度
+            chunk_len = len(chunk)
+            if chunk_len < frame_bytes_size:
+                # 补0
+                chunk +=  b'\x00' * (frame_bytes_size - chunk_len)
+
+            np_frame = np.frombuffer(chunk, dtype=np.int16)
+
+            np_bytes = np_frame.tobytes()
+
+            opus_data = encoder.encode(np_bytes, frame_num)
+
+            opus_datas.append(opus_data)
+
+        return opus_datas, duration
+
+if __name__ == "__main__":
+    opus = Opus_Encode()
+    opus_datas, duration = opus.audio_to_opus("../../test.mp3")
+    print(f"音频总时长为：{duration} 秒")
+    print(f"音频OPUS数据大小为：{ len(opus_datas) } 字节")
+    print()
